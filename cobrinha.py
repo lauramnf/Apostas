e34034a911b1079d5e36f9c2975df74e807bbfe2
Laura Maciel <laura.neves2008@hotmail.com>
2020-11-07 16:38:02 -0200

    Definindo movimentos

--- cobrinha.py
+++ cobrinha.py
@@ -1,5 +1,14 @@
 import turtle
 
+def frente():
+	cursor.fd(20)
+def tras():
+	cursor.bk(20)
+def  direita():
+	cursor.rt(90)
+def  esquerda():
+	cursor.lt(90)
+
 #Preparando a tela
 tela = turtle.Screen()
 tela.title('Joguinho da cobrinha')
@@ -8,9 +17,14 @@ tela.setup(width = 600, height = 600)
 
 #Definindo um objeto Turtle (minha cobra)
 cursor = turtle.Turtle()
-cursor.penup()
 
-cursor.fd(60)
+#Não deixando rastro e relacionando movimentos com setas
+cursor.penup()
+turtle.onkeypress(frente, 'Up')
+turtle.onkeypress(tras, 'Down')
+turtle.onkeypress(direita, 'Right')
+turtle.onkeypress(esquerda, 'Left')
+turtle.listen()
 
 turtle.Screen().exitonclick()
 
