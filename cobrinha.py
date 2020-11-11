e3d56bf3ea32636d90532714cdf73a156704760c
Laura Maciel <laura.neves2008@hotmail.com>
2020-11-11 00:29:17 -0200

    Comendo comida

--- cobrinha.py
+++ cobrinha.py
@@ -3,12 +3,36 @@ import random
 
 def cima():
 	cursor.sety(cursor.ycor()+20)
+	if cursor.distance(comida) < 20:
+		rand1 = random.randrange(-300, 300)
+		rand2 = random.randrange(-300, 300)
+		comida.penup()
+		comida.speed(0)
+		comida.goto((rand1, rand2))
 def baixo():
 	cursor.sety(cursor.ycor()-20)
+	if cursor.distance(comida) < 20:
+		rand1 = random.randrange(-300, 300)
+		rand2 = random.randrange(-300, 300)
+		comida.penup()
+		comida.speed(0)
+		comida.goto((rand1, rand2))
 def  direita():
 	cursor.setx(cursor.xcor()+20)
+	if cursor.distance(comida) < 20:
+		rand1 = random.randrange(-300, 300)
+		rand2 = random.randrange(-300, 300)
+		comida.penup()
+		comida.speed(0)
+		comida.goto((rand1, rand2))
 def  esquerda():
 	cursor.setx(cursor.xcor()-20)
+	if cursor.distance(comida) < 20:
+		rand1 = random.randrange(-300, 300)
+		rand2 = random.randrange(-300, 300)
+		comida.penup()
+		comida.speed(0)
+		comida.goto((rand1, rand2))
 
 #Preparando a tela
 tela = turtle.Screen()
@@ -16,9 +40,18 @@ tela.title('Joguinho da cobrinha')
 tela.bgcolor('#add8e6')
 tela.setup(width = 600, height = 600)
 
+comida = turtle.Turtle()
+comida.color('yellow', 'yellow')
+comida.shape('circle')
+rand1 = random.randrange(-290, 290)
+rand2 = random.randrange(-290, 290)
+comida.penup()
+comida.speed(0)
+comida.goto((rand1, rand2))
 #Definindo um objeto Turtle (minha cobra)
 cursor = turtle.Turtle()
-cursos.shape('square')
+cursor.shape('square')
+cursor.speed(0)
 
 #Não deixando rastro e relacionando movimentos com setas
 cursor.penup()
@@ -28,14 +61,6 @@ turtle.onkeypress(direita, 'Right')
 turtle.onkeypress(esquerda, 'Left')
 turtle.listen()
 
-bolinha = turtle.Turtle()
-bolinha.color('yellow', 'yellow')
-bolinha.shape('circle')
-rand1 = random.randrange(-300, 300)
-rand2 = random.randrange(-300, 300)
-bolinha.penup()
-bolinha.speed(0)
-bolinha.setpos((rand1, rand2))
 
 
 turtle.Screen().exitonclick()
