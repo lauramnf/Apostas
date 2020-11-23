d149df8eca2744940c2c3c8d09e740a0dd5cacc4
Laura Maciel <laura.neves2008@hotmail.com>
2020-11-23 16:48:47 -0200

    Add search for the package status

--- has_arrived.py
+++ has_arrived.py
@@ -4,3 +4,7 @@ import re
 #Download page
 response = requests.get(url)
 response.raise_for_status() #if error it will stop the program
+
+#Search for package's status
+status = re.search(r'(<td width="100%" colspan="3" style="color: darkorange; font-size: 17px;"><center><b>)(.*)(</b></center></td>)', response.text)
+print(status[2])
